55a51ed7ff2e627bed7c76cf61e01145e3091eb6
yenifersan <44785957+yenifersan@users.noreply.github.com>
2019-09-30 15:35:03 -0500

    Agregar archivos de proyecto.

---
+++ Calf01/Calf01/inicio.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calf01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class inicio : ContentPage
+    {
+        public inicio()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await App.Modificador.Detail.Navigation.PushAsync(new StackLayoutDemo());
+                App.Modificador.IsPresented = false;
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await App.Modificador.Detail.Navigation.PushAsync(new Grid());
+                App.Modificador.IsPresented = false;
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await App.Modificador.Detail.Navigation.PushAsync(new PantallaItemTres());
+                App.Modificador.IsPresented = false;
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await App.Modificador.Detail.Navigation.PushAsync(new PantallaItemCuatro());
+                App.Modificador.IsPresented = false;
+            };
+
+
+
+        }
+    }
+}
