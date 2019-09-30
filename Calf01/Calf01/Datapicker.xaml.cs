f537e65df3a070fbdc1940060c279962c4692dd6
yenifersan <44785957+yenifersan@users.noreply.github.com>
2019-09-30 16:49:54 -0500

    commit

--- Calf01/Calf01/Datapicker.xaml.cs
+++ Calf01/Calf01/Datapicker.xaml.cs
@@ -16,5 +16,24 @@ namespace Calf01
         {
             InitializeComponent();
         }
+
+        void OnDateSelected(object sender, DateChangedEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void Recalculate()
+        {
+            TimeSpan timeSpan = endDatePicker.Date - startDatePicker.Date +
+                (includeSwitch.IsToggled ? TimeSpan.FromDays(1) : TimeSpan.Zero);
+
+            resultLabel.Text = String.Format("{0} day{1} between dates",
+                                                timeSpan.Days, timeSpan.Days == 1 ? "" : "s");
+        }
     }
 }
